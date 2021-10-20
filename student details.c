9d3386b3c081d02f90af81e35bb6bd2f01134095
RakurthiDeviSriHari <92290667+RakurthiDeviSriHari@users.noreply.github.com>
2021-10-20 12:31:08 +0530

    Add files via upload

---
+++ student details.c
@@ -0,0 +1,17 @@
+// c program to print details of student
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float CGPA;
+	char initial;
+	// initialization
+	age=19;
+	CGPA=8.57;
+	initial='R';
+	// output
+	printf("student age:%d \n",age);
+	printf("student CGPA:%f \n",CGPA);
+	printf("student initial starts with:%c",initial); 
+}
---
+++ sum scanf.c
@@ -0,0 +1,10 @@
+/* c program to print sum using scanf */
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter two numbers:");
+	scanf("%d %d",&a,&b);
+	printf("sum of%d and %d is%d",a,b,a+b);
+}
+          
