ca708c53bd9ff9c22a4328eae27b7654e21e266c
RakurthiDeviSriHari <92290667+RakurthiDeviSriHari@users.noreply.github.com>
2021-10-25 11:43:42 +0530

    Add files via upload

---
+++ sudent marks.c
@@ -0,0 +1,17 @@
+// constructa program for student marks  
+#include<stdio.h>
+void main()
+{
+	int phy,che,mat,eng,tot;
+	phy=50;
+	che=60;
+	mat=70;
+	eng=80;
+	tot= phy+che+mat+eng;
+	printf("physics marks:%d\n",phy);
+	printf("chemistry marks:%d\n",che);
+	printf("maths marks:%d\n",mat);
+	printf("English marks:%d\n",eng); 
+	printf("Total marks:%d",tot);
+	
+}
