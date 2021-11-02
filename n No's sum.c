3205ad4cdbb7bd78496f5c6be494d092aaa064db
RakurthiDeviSriHari <92290667+RakurthiDeviSriHari@users.noreply.github.com>
2021-11-02 12:26:07 +0530

    Add files via upload

---
+++ n No's sum.c
@@ -0,0 +1,11 @@
+// sum of n naturalnumbers
+#include<stdio.h>
+void main()
+{
+    int n,sum;
+    printf("Enter number:");
+    scanf("%d",&n);
+    sum= n*(n+1)/2;
+    printf("sum of %d natural numbers:%d",n,sum);
+  	
+}
