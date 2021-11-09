2c1769ed936499559d5c1cea6deb1b2720296e71
RakurthiDeviSriHari <92290667+RakurthiDeviSriHari@users.noreply.github.com>
2021-11-09 12:28:34 +0530

    Add files via upload

---
+++ ascii1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+    printf("enter no:");
+	scanf("%d",&n);
+	printf("corresponding ASCII character for %d is:%c",n,n); 
+	
+}
---
+++ ascii2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	char n;
+    printf("enter character:");
+	scanf("%c",&n);
+	printf("corresponding ASCII value for %c is:%d",n,n);
+}
+
+	
+	
